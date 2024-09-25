4a238fd3853155bf51eb12ceac2a940e5505a800
Bruno Panizzi <bruno@ibotiadvogados.com.br>
2024-09-25 14:22:54 -0300

    create sales service

---
+++ app/services/SalesService.ts
@@ -0,0 +1,19 @@
+import { db } from "~/db"
+import { newSaleSchema, sale, type NewSale as DbNewSale } from "~/db/schema"
+
+export type NewSale = DbNewSale
+export { newSaleSchema }
+
+class SalesService {
+  async index() {
+    return await db.query.sale.findMany()
+  }
+
+  async create(newSale: NewSale) {
+    const createdSale = await db.insert(sale).values(newSale).returning()
+
+    return createdSale
+  }
+}
+
+export default new SalesService()
