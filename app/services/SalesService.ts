3cd3f0d507d7becc98df9b1cbf39d2bdbda8e67d
Bruno Panizzi <bruno@ibotiadvogados.com.br>
2024-09-26 15:04:18 -0300

    add queries by date

--- app/services/SalesService.ts
+++ app/services/SalesService.ts
@@ -1,3 +1,5 @@
+import { endOfMonth, startOfMonth } from "date-fns"
+
 import { db } from "~/db"
 import { newSaleSchema, sale, type NewSale as DbNewSale } from "~/db/schema"
 
@@ -5,9 +7,77 @@ export type NewSale = DbNewSale
 export { newSaleSchema }
 
 class SalesService {
+  private validateDate(month: number, year: number): Date {
+    if (month < 1 || month > 12 || year < 2000) {
+      throw new Error("date out of range")
+    }
+
+    return new Date(year, month - 1)
+  }
+
   async index() {
     return await db.query.sale.findMany({
       with: { seller: { columns: { name: true } } },
+    })
+  }
+
+  async getBySeller(sellerId: string) {
+    await db.query.sale.findMany({
+      where: (sales, { eq }) => eq(sales.seller, sellerId),
+    })
+  }
+
+  async getByMonth(month: number, year: number) {
+    const date = this.validateDate(month, year)
+
+    return await db.query.sale.findMany({
+      where: (sales, { between }) =>
+        between(
+          sales.date,
+          startOfMonth(date).toDateString(),
+          endOfMonth(date).toDateString(),
+        ),
+      with: {
+        seller: { columns: { name: true } },
+      },
+    })
+  }
+
+  async getByMonthAndUser(month: number, year: number, userId: string) {
+    const date = this.validateDate(month, year)
+
+    return await db.query.sale.findMany({
+      where: (sales, { between, and, eq }) =>
+        and(
+          eq(sales.seller, userId),
+          between(
+            sales.date,
+            startOfMonth(date).toDateString(),
+            endOfMonth(date).toDateString(),
+          ),
+        ),
+      with: {
+        seller: { columns: { name: true } },
+      },
+    })
+  }
+
+  async getNewClientsByMonth(month: number, year: number) {
+    const date = this.validateDate(month, year)
+
+    return await db.query.sale.findMany({
+      where: (sales, { between, and, eq }) =>
+        and(
+          eq(sales.isRepurchase, false),
+          between(
+            sales.date,
+            startOfMonth(date).toDateString(),
+            endOfMonth(date).toDateString(),
+          ),
+        ),
+      with: {
+        seller: { columns: { name: true } },
+      },
     })
   }
 
