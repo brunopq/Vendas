b1a96317caf3c6c8eb1a98618567334d63feb409
Bruno Panizzi <bruno@ibotiadvogados.com.br>
2024-10-01 13:19:06 -0300

    add toaster

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,8 @@ import type { LinksFunction } from "@remix-run/node"
 
 import "./tailwind.css"
 
+import { Toaster } from "./components/ui/toaster"
+
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
@@ -33,6 +35,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         {children}
+        <Toaster />
         <ScrollRestoration />
         <Scripts />
       </body>
