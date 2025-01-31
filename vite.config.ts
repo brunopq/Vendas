38f038274ffbf50903aa60b9db569cd72491af08
Bruno Panizzi <bruno@ibotiadvogados.com.br>
2025-01-31 15:20:11 -0300

    add hono server to simplify deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite"
 import { reactRouter } from "@react-router/dev/vite"
+import { reactRouterHonoServer } from "react-router-hono-server/dev"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-  build: {
-    target: "esnext",
-  },
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [
+    reactRouterHonoServer({ runtime: "node" }),
+    reactRouter(),
+    tsconfigPaths(),
+  ],
 })
